22b823636b12fe5294468f6ab212b49cd0a65614
Hanchon <guillermo.paoletti@gmail.com>
2022-03-03 22:32:20 +0100

    pointing to mainnet

--- src/utils/contants.tsx
+++ src/utils/contants.tsx
@@ -2,4 +2,4 @@ export const REACT_APP_BACKEND_URL =
     process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000';
 
 export const REACT_APP_NODE_URL =
-    process.env.REACT_APP_NODE_URL || 'http://35.85.238.134:1317';
+    process.env.REACT_APP_NODE_URL || 'https://rest.bd.evmos.org:1317';
